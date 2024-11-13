8bb752770105f9e3e5c1ac5daf0fc5ed6e2b8a62
Usman Saeed <uusaeed749@gmail.com>
2024-11-13 23:56:09 +0500

    best seller component

--- src/components/ProductItem.jsx
+++ src/components/ProductItem.jsx
@@ -9,7 +9,7 @@ const ProductItem = ({ id, image, name, price }) => {
             <div className='overflow-hidden'>
                 <img className='hover:scale-110 transition ease-in-out' src={image[0]} alt='' />
             </div>
-            <p className='pt-3 pb-1 text-sm'>{name}</p>
+            <p className='pt-3 pb-1 text-xs text-gray-800'>{name}</p>
             <p className='text-sm font-medium'>{currency}{price}</p>
         </Link>
     )

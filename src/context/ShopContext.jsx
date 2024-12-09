59683e926ea55db4436f984b5ad0a957756bac10
Usman Saeed <uusaeed749@gmail.com>
2024-12-09 20:27:44 +0500

    add to cart functionality done

--- src/context/ShopContext.jsx
+++ src/context/ShopContext.jsx
@@ -1,5 +1,6 @@
 import { createContext, useState } from "react";
 import { products } from '../assets/assets'
+import { toast } from "react-toastify";
 
 const ShopContext = createContext();
 
@@ -8,8 +9,58 @@ const ShopContextProvider = (props) => {
     const delivery_fee = 10;
     const [search, setSearch] = useState('');
     const [showSearch, setShowSearch] = useState(false);
+    const [cartItems, setCartItems] = useState({});
+
+    const addToCart = async (itemId, size) => {
+        if (!size) {
+            toast.error('Select product size');
+            return;
+        }
+
+        let cartData = structuredClone(cartItems);
+        if (cartData[itemId]) {
+            if (cartData[itemId][size]) {
+                cartData[itemId][size] += 1;
+            }
+            else {
+                cartData[itemId][size] = 1;
+            }
+        }
+        else {
+            cartData[itemId] = {};
+            cartData[itemId][size] = 1;
+        }
+        setCartItems(cartData);
+    }
+
+
+    const getCartCount = () => {
+        let totalCount = 0;
+        for (const items in cartItems) {
+            for (const item in cartItems[items]) {
+                try {
+                    if (cartItems[items][item] > 0) {
+                        totalCount += cartItems[items][item];
+                    }
+                } catch (error) {
+                    console.error(error);
+                }
+            }
+        }
+        return totalCount;
+    }
+
     const value = {
-        products, currency, delivery_fee, search, setSearch, showSearch, setShowSearch
+        products,
+        currency,
+        delivery_fee,
+        search,
+        setSearch,
+        showSearch,
+        setShowSearch,
+        cartItems,
+        addToCart,
+        getCartCount
     }
 
     return (<ShopContext.Provider value={value}>
